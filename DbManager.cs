024e2d23172df627266c69a555566e6b2e7c9449
Douglas Hammarstam <dogge20@hotmail.com>
2020-09-30 10:18:47 +0200

    database entires inster into works

---
+++ DbManager.cs
@@ -0,0 +1,33 @@
+using System;
+using Npgsql;
+
+namespace database.manager {
+    public class DbManager {
+
+        public string ConnectionString { get; set; }
+        public NpgsqlConnection Connection { get; set; }
+
+        public DbManager () {
+            this.ConnectionString = "Host=127.0.0.1;Username=postgres;Password=postgres;Database=postgres";
+            this.Connection = new NpgsqlConnection (this.ConnectionString);
+
+        }
+
+        public void Connect () => this.Connection.Open ();
+
+        public void INSERTINTOMENU (int weekday, string name, string ingredients, int price, int restaurantId) {
+            this.Connect ();
+
+            var sql = $"INSERT INTO MENU (WEEKDAY, MENU_NAME, INGREDIENTS, PRICE, RESTAURANT_ID) VALUES ({weekday}, '{name}', '{ingredients}', {price}, {restaurantId})";
+
+            System.Console.WriteLine (sql);
+
+            var cmd = new NpgsqlCommand (sql, this.Connection);
+
+            var result = cmd.ExecuteScalar ().ToString ();
+
+            System.Console.WriteLine ("RESULTAS: " + result);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,39 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
+using database.manager;
+using Dbitems.MenuItem;
+using Newtonsoft;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-namespace afalunchdb
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(queue: "hello",
-                    durable: false,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null);
+namespace afalunchdb {
+    class Program {
+        static void Main (string[] args) {
 
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] Received {0}", message);
+            DbManager dbManager = new DbManager (); 
+            List<MenuItem> menuItems = null;
+
+            var factory = new ConnectionFactory () { HostName = "localhost" };
+            using (var connection = factory.CreateConnection ())
+            using (var channel = connection.CreateModel ()) {
+                channel.QueueDeclare (queue: "insertveckansluncher",
+                    durable : false,
+                    exclusive : false,
+                    autoDelete : false,
+                    arguments : null);
+
+                var consumer = new EventingBasicConsumer (channel);
+                consumer.Received += (model, ea) => {
+                    var body = ea.Body.ToArray ();
+                    var message = Encoding.UTF8.GetString (body);
+                    Console.WriteLine (" [x] Received {0}", message);
+                    menuItems = Newtonsoft.Json.JsonConvert.DeserializeObject<List<MenuItem>> (message);
+                    System.Console.WriteLine("MENU ITEMS LIST " + menuItems.Count);
+
                 };
-                channel.BasicConsume(queue: "hello",
-                    autoAck: true,
-                    consumer: consumer);
+                channel.BasicConsume (queue: "insertveckansluncher",
+                    autoAck : true,
+                    consumer : consumer);
 
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
+                Console.WriteLine (" Press [enter] to exit.");
+                Console.ReadLine ();
 
             }
+
+
+       
+           //dbManager.INSERTINTORESTAURANG ();
         }
     }
 }
