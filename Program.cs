ad34153f76516ba80aa9dac9329132c82bd39fe3
Kamal <kamal.al-wendawi@afaforsakring.se>
2020-09-30 13:22:52 +0200

    RECEIVE IMOLO AND CLEANER CODE

--- Program.cs
+++ Program.cs
@@ -6,51 +6,16 @@ using Dbitems.MenuItem;
 using Newtonsoft;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using Receiver.Diwine;
+using Receiver.Ilmolo;
 
 namespace afalunchdb {
     class Program {
         static void Main (string[] args) {
-
-            DbManager dbManager = new DbManager ();
-            Dictionary<int, List<MenuItem>> menuItems = null;
-            string jsonAllDays = "";
-
-            var factory = new ConnectionFactory () { HostName = "localhost" };
-            using (var connection = factory.CreateConnection ())
-            using (var channel = connection.CreateModel ()) {
-                channel.QueueDeclare (queue: "insertveckansluncher",
-                    durable : false,
-                    exclusive : false,
-                    autoDelete : false,
-                    arguments : null);
-
-                var consumer = new EventingBasicConsumer (channel);
-                consumer.Received += (model, ea) => {
-                    var body = ea.Body.ToArray ();
-                    var message = Encoding.UTF8.GetString (body);
-                    //Console.WriteLine (" [x] Received {0}", message);
-                    jsonAllDays = message;
-
-                };
-                channel.BasicConsume (queue: "insertveckansluncher",
-                    autoAck : true,
-                    consumer : consumer);
-
-                Console.WriteLine (" Press [enter] to exit.");
-                Console.ReadLine ();
-
-            }
-            //Receive the json obj
-            menuItems = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<int, List<MenuItem>>> (jsonAllDays);
-            //Print json obj
-            foreach (var item in menuItems) {
-                System.Console.WriteLine(item.Key);
-                foreach(var lunchitem in item.Value){
-                    System.Console.WriteLine(lunchitem.Title);
-                    dbManager.INSERTINTOMENU(item.Key, lunchitem.Title, lunchitem.Ingredients, lunchitem.Price, lunchitem.IdOfRestaurant);
-                }
-            }
-            //dbManager.INSERTINTORESTAURANG ();
+            DiwineReceiver diwine = new DiwineReceiver();
+            diwine.ReceiverOfDiwine();
+            IlmoloReceiver ilmolo = new IlmoloReceiver();
+            ilmolo.ReceiverOfIlmolo();
         }
     }
 }
